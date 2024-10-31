485e072cba716cb728627d7501afd6bba0d34006
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-10-31 11:10:31 +0330

    OnlineOrderService is adjusted to the additions of messaging service

--- .idea/src/PaymentServices/OnlineOrderService.java
+++ .idea/src/PaymentServices/OnlineOrderService.java
@@ -1,6 +1,13 @@
 package PaymentServices;
+import MessagingServices.MessagingService;
 
 public class OnlineOrderService implements OrderService {
+    private MessagingService messagingService;
+
+    public OnlineOrderService(MessagingService messagingService) {
+        this.messagingService = messagingService;
+    }
+
     @Override
     public void onSiteOrderRegister(String customerName) {
         //Empty Body!
@@ -9,6 +16,7 @@ public class OnlineOrderService implements OrderService {
     @Override
     public void onlineOrderRegister(String customerName) {
         System.out.println("online order registered for " + customerName);
+        messagingService.sendMessage("Order registered for " + customerName);
     }
 
     @Override
@@ -19,6 +27,6 @@ public class OnlineOrderService implements OrderService {
     @Override
     public void onlineOrderPayment(int foodPrice) {
         System.out.println("online Payment with Price : " + foodPrice + " Tomans!");
+        messagingService.sendMessage("Payment completed for " + foodPrice + " Tomans!");
     }
-
 }
