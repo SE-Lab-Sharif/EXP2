209f990f1bfa9ba43725110f57b8dcc65761d849
soheilnazari2001 <soheil.nazari@sharif.edu>
2024-11-03 02:35:41 +0330

    I regret it deeply

--- Step_1_Non_SOLID/MessagingServices/TelegramService.java
+++
@@ -1,9 +0,0 @@
-package MessagingServices;
-
-public class TelegramService implements MessagingService {
-
-    @Override
-    public void sendMessage(String message) {
-        System.out.println("Sending message to Telegram: " + message);
-    }
-}
